d67a31b6c7093b7a181d620b950a2025de33fb39
MichaelOli123 <oliveira3173@mydusd.org>
2025-02-03 09:19:39 -0800

    rounded the mean value and some cleaning

--- MeteoriteAnalysis.java
+++ MeteoriteAnalysis.java
@@ -32,13 +32,14 @@ public class MeteoriteAnalysis {
         
         // Compute mean mass
         double meanMass = masses.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
+        String meanMassString = String.format("%.2f", meanMass);
         
         // Find years with most and least meteorites
         int maxYear = Collections.max(yearCounts.entrySet(), Map.Entry.comparingByValue()).getKey();
         int minYear = Collections.min(yearCounts.entrySet(), Map.Entry.comparingByValue()).getKey();
         
         // Output results
-        System.out.println("Mean mass of meteorites: " + meanMass + " g");
+        System.out.println("Mean mass of meteorites: " + meanMassString + " g");
         System.out.println("Year with most meteorite falls: " + maxYear);
         System.out.println("Year with least meteorite falls: " + minYear);
     }
